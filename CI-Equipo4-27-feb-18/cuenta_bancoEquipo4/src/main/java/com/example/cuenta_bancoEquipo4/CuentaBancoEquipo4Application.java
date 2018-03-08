ad48d1a53b67ef05b8e439f6fc51e4f404b34659
svhector <nostgrand@gmail.com>
2018-03-08 09:57:41 -0600

    Volviendo a crear proyecto

---
+++ CI-Equipo4-27-feb-18/cuenta_bancoEquipo4/src/main/java/com/example/cuenta_bancoEquipo4/CuentaBancoEquipo4Application.java
@@ -0,0 +1,12 @@
+package com.example.cuenta_bancoEquipo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaBancoEquipo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaBancoEquipo4Application.class, args);
+	}
+}
---
+++ CI-Equipo4-27-feb-18/cuenta_bancoEquipo4/src/test/java/com/example/cuenta_bancoEquipo4/CuentaBancoEquipo4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.cuenta_bancoEquipo4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CuentaBancoEquipo4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
