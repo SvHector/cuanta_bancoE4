d8dc8b33cbb0651bcc9a1175b0d5c18894f70c07
svhector <nostgrand@gmail.com>
2018-03-09 11:42:57 -0600

    Google OAuth2

--- CI-Equipo4-27-feb-18/proyectoBancoE4/regClienteOaut2/src/main/java/com/equipo4/cuentaBanco/regClienteOaut2/RegClienteOaut2Application.java
+++ CI-Equipo4-27-feb-18/proyectoBancoE4/regClienteOaut2/src/main/java/com/equipo4/cuentaBanco/regClienteOaut2/RegClienteOaut2Application.java
@@ -1,12 +1,23 @@
 package com.equipo4.cuentaBanco.regClienteOaut2;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RegClienteOaut2Application {
-
+ 	
 	public static void main(String[] args) {
 		SpringApplication.run(RegClienteOaut2Application.class, args);
+	}
+	
+	@RequestMapping(value="/user")
+	public Principal user(Principal principal) {
+		return principal;
+		
 	}
 }
---
+++ CI-Equipo4-27-feb-18/proyectoBancoE4/regClienteOaut2/src/main/java/com/equipo4/cuentaBanco/regClienteOaut2/WebSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.equipo4.cuentaBanco.regClienteOaut2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+
+@Configuration
+@EnableOAuth2Client
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	                .csrf()
+	                    .disable()
+	                .antMatcher("/**")
+	                .authorizeRequests()
+	                .antMatchers("/", "/index.html")
+	                    .permitAll()
+	                .anyRequest()
+	                    .authenticated();
+	    }
+}
