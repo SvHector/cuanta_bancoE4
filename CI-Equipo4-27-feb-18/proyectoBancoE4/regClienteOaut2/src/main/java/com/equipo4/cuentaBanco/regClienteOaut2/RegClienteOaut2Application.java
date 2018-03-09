d7512cc3baa60136be8f0fe588f954e8c98c2e16
svhector <nostgrand@gmail.com>
2018-03-09 09:59:58 -0600

    Proyecto para probar OAuth2

---
+++ CI-Equipo4-27-feb-18/proyectoBancoE4/regClienteOaut2/src/main/java/com/equipo4/cuentaBanco/regClienteOaut2/RegClienteOaut2Application.java
@@ -0,0 +1,12 @@
+package com.equipo4.cuentaBanco.regClienteOaut2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegClienteOaut2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegClienteOaut2Application.class, args);
+	}
+}
--- CI-Equipo4-27-feb-18/proyectoBancoE4/regClienteOaut2/src/test/java/com/equipo4/cuentaBanco/regClienteOaut2/RegClienteOaut2ApplicationTests.java
+++ CI-Equipo4-27-feb-18/proyectoBancoE4/regClienteOaut2/src/test/java/com/equipo4/cuentaBanco/regClienteOaut2/RegClienteOaut2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.cuenta_bancoEquipo4;
+package com.equipo4.cuentaBanco.regClienteOaut2;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CuentaBancoEquipo4ApplicationTests {
+public class RegClienteOaut2ApplicationTests {
 
 	@Test
 	public void contextLoads() {
