34dc8a7b0bf6da9b14fafb4ccd5afdc1fa2f631e
svhector <nostgrand@gmail.com>
2018-03-09 11:48:08 -0600

    Commit

--- CI-Equipo4-27-feb-18/proyectoBancoE4/regClienteOaut2/src/test/java/com/equipo4/cuentaBanco/regClienteOaut2/RegClienteOaut2ApplicationTests.java
+++ CI-Equipo4-27-feb-18/proyectoBancoE4/regClienteOaut2/src/test/java/com/equipo4/cuentaBanco/regClienteOaut2/RegClienteOaut2ApplicationTests.java
@@ -1,16 +1,16 @@
-package com.equipo4.cuentaBanco.regClienteOaut2;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class RegClienteOaut2ApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.equipo4.cuentaBanco.regClienteOaut2;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class RegClienteOaut2ApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
