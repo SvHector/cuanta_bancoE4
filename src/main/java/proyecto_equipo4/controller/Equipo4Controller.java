d45df3a58d2f72e13b23d3caa26fcaeb629d8fbf
svhector <nostgrand@gmail.com>
2018-03-09 15:53:00 -0600

    Tratando de salvar la vida

--- src/main/java/proyecto_equipo4/controller/Equipo4Controller.java
+++ src/main/java/proyecto_equipo4/controller/Equipo4Controller.java
@@ -1,16 +1,16 @@
 package proyecto_equipo4.controller;
 
-import proyecto_equipo4.model.Client4;
-import proyecto_equipo4.repository.RepositoryCLient;
+//import proyecto_equipo4.model.Client4;
+//import proyecto_equipo4.repository.RepositoryCLient;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
+//import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.jpa.repository.JpaRepository;
 
 @Controller
 public class Equipo4Controller 
